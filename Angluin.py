9601c5908745d679402c6d103ce992771bcd0348
carla <carla.segalie@etu.univ-amu.fr>
2023-03-07 23:12:56 +0100

    Méthodes dans l'ordre dans Angluin.py

--- Angluin.py
+++ Angluin.py
@@ -1,6 +1,59 @@
 from automata.fa.dfa import DFA
 
 
+def Lstar_Initialise():
+    red = []
+    red.append('epsilon')
+    blue = []
+    for l in alphabet:
+        blue.append(l)
+    exp = []
+    exp.append('epsilon')
+    n = len(red)  # nombre de lignes
+    m = len(exp)  # nombre de colonnes
+    M = 'teste d equivalence'
+    table = [['' for j in range(m)] for i in range(n)]
+    print(table)
+    table['epsilon']['epsilon'] = M('epsilon')
+    for lettre in alphabet:
+        table[lettre]['epsilon'] = M(lettre)
+    return table
+
+
+print(Lstar_Initialise())
+
+
+def compareOT(mq, exp, u, v):
+    for e in exp:
+        if mq[str(u + e)] != mq[str(v + e)]:
+            return False
+    return True
+
+
+"""
+Allows to make our automaton's table close ?
+input = the table corresponding to the actual automaton
+output = the updating table corresponding to the new actual automaton
+-- uses function membership_test
+"""
+
+
+def blue(pref):
+    blue = []
+    for i in pref:
+        if pref[i] == "blue":
+            blue.append(i)
+    return blue
+
+
+def red(pref):
+    red = []
+    for i in pref:
+        if pref[i] == "red":
+            red.append(i)
+    return red
+
+
 def is_closed(mq, pref, exp):
     for s in blue(pref):
         for u in red(pref):
@@ -14,43 +67,15 @@ def is_consistent(mq, pref, exp):
         for s2 in red(pref):
             if compareOT(mq, exp, s1, s2):
                 for a in alphabet:
-                    if not compareOT(mq, exp, str(s1+a), str(s2+a)):
+                    if not compareOT(mq, exp, str(s1 + a), str(s2 + a)):
                         return False
     return True
 
 
-"""
-Allows to make our automaton's table consistent 
-input = the table corresponding to the actual automaton
-output = the updating table corresponding to the new actual automaton
--- uses function membership_test
-"""
-def find_consistency_problem(mq, pref, exp):
-    for s1 in pref:
-        for s2 in pref:
-            if pref[s1] == "red" and pref[s2] == "red":
-                if compareOT(mq, exp, s1, s2):
-                    for e in exp:
-                        for a in alphabet:
-                            if mq[str(s1 + a + e)] != mq[str(s2 + a + e)]:
-                                return a, e
-    return False
-# on a pas eu le temps de modifier mais peut etre que ça sert à rien de faire deux fois tout
-# le parcours : on peut fusionner find_consistency_problem et is_consistent
-
-
-def lstar_consistent(mq, pref, exp):
-    a, e = find_consistency_problem(mq, pref, exp)
-    exp.add(str(a + e))
-    for line in pref:
-        for e in exp:
-            if str(line+e) not in mq:
-                mq[str(line+e)] = '*' # membership_test(mq, pref, exp)
-
-
-def compareOT(mq, exp, u, v):
-    for e in exp:
-        if mq[str(u+e)] != mq[str(v+e)] :
+def different(mq, pref, exp, s):
+    dernier = exp[len(exp) - 1]
+    for u in red(pref):
+        if mq[s + dernier] == mq[u + dernier]:
             return False
     return True
 
@@ -66,27 +91,60 @@ output = the updating table corresponding to the same automaton but filled
             if mq[x+y] == '*':
                 res = automate.accepts(x+y)
                 mq[x+y] = res
-    return mq, pref, T""" # j'ai fais bordel mais je le laisse lol
+    return mq, pref, T"""  # j'ai fais bordel mais je le laisse lol
 
 
+def membership_test(u):
+    return 0
 
-alphabet = [] # j'ai mis pour que ma fonction me fasse pas la tête mais on peut changer ou enlever
+
+def lstar_close(mq, pref, exp, alphabet):
+    dernier = exp[len(exp) - 1]
+    for s in blue(pref):
+        if different(mq, pref, exp, s):
+            pref[s] = "red"
+            for a in alphabet:
+                mq[s + a + dernier] = membership_test(s + a + dernier)
+                pref[s + a] = "blue"
+    return mq, pref, exp
 
 
-def LSTAR_USEEQ(mq, pref, exp, answer):
-    prefixes = get_prefixes(answer)
-    for p in prefixes:
-        pref[p] = "red"
-        for a in alphabet:
-            if str(p+a) not in prefixes:
-                pref[str(p+a)] = "blue"
-    # for line in pref.keys():
-    # for line in list(pref.keys()):
-    for line in [*pref]:
+"""
+Allows to make our automaton's table consistent 
+input = the table corresponding to the actual automaton
+output = the updating table corresponding to the new actual automaton
+-- uses function membership_test
+"""
+
+
+def find_consistency_problem(mq, pref, exp):
+    for s1 in pref:
+        for s2 in pref:
+            if pref[s1] == "red" and pref[s2] == "red":
+                if compareOT(mq, exp, s1, s2):
+                    for e in exp:
+                        for a in alphabet:
+                            if mq[str(s1 + a + e)] != mq[str(s2 + a + e)]:
+                                return a, e
+    return False
+
+
+# on a pas eu le temps de modifier mais peut etre que ça sert à rien de faire deux fois tout
+# le parcours : on peut fusionner find_consistency_problem et is_consistent
+
+
+def lstar_consistent(mq, pref, exp):
+    a, e = find_consistency_problem(mq, pref, exp)
+    exp.add(str(a + e))
+    for line in pref:
         for e in exp:
-            if str(line+e) not in mq:
-                mq[str(line+e)] = membership_test(str(line+e)) # nom temporaire selon comment on fait pour les requetes d'appartenance
-    # return mq, pref, exp # j'allais le mettre mais enft ça modifie direct je pense (j'espère)
+            if str(line + e) not in mq:
+                mq[str(line + e)] = '*'  # membership_test(mq, pref, exp)
+
+
+# A implementer, demander a Harry
+def equivalence_test(mq, pref, exp):
+    return True
 
 
 """
@@ -94,9 +152,26 @@ Renvoie les préfixes d'un mot sous forme de liste. dsl je sais pas faire la doc
 """
 def get_prefixes(word):
     prefixes = []
-    for i in range(len(word)+1):
+    for i in range(len(word) + 1):
         prefixes.append(word[0:i])
     return prefixes
+
+
+def LSTAR_USEEQ(mq, pref, exp, answer):
+    prefixes = get_prefixes(answer)
+    for p in prefixes:
+        pref[p] = "red"
+        for a in alphabet:
+            if str(p + a) not in prefixes:
+                pref[str(p + a)] = "blue"
+    # for line in pref.keys():
+    # for line in list(pref.keys()):
+    for line in [*pref]:
+        for e in exp:
+            if str(line + e) not in mq:
+                mq[str(line + e)] = membership_test(
+                    str(line + e))  # nom temporaire selon comment on fait pour les requetes d'appartenance
+    # return mq, pref, exp # j'allais le mettre mais enft ça modifie direct je pense (j'espère)
 
 
 """
@@ -115,7 +190,7 @@ def lstar_buildautomaton(mq, pref, exp, alphabet):
             Q.add(u)
     F_a = set()
     delta = {}
-    for q_u in Q :
+    for q_u in Q:
         if mq[str(q_u)] == 1:
             F_a.add(q_u)
         delta[q_u] = {}
@@ -128,79 +203,9 @@ def lstar_buildautomaton(mq, pref, exp, alphabet):
     return DFA(states=Q, input_symbols=alphabet, transitions=delta, initial_state="", final_states=F_a)
 
 
-"""
-Allows to make our automaton's table close ?
-input = the table corresponding to the actual automaton
-output = the updating table corresponding to the new actual automaton
--- uses function membership_test
-"""
-def blue(pref):
-    blue = []
-    for i in pref:
-        if pref[i] == "blue":
-            blue.append(i)
-    return blue
-
-
-def red(pref):
-    red = []
-    for i in pref:
-        if pref[i] == "red":
-            red.append(i)
-    return red
-
-
-def different(mq, pref, exp, s):
-    dernier = exp[len(exp) - 1]
-    for u in red(pref):
-        if mq[s + dernier] == mq[u + dernier]:
-            return False
-    return True
-
-
-def membership_test(u):
-    return 0
-
-
-def lstar_close(mq, pref, exp, alphabet):
-    dernier = exp[len(exp) - 1]
-    for s in blue(pref):
-        if different(mq, pref, exp, s):
-            pref[s] = "red"
-            for a in alphabet :
-                mq[s + a + dernier] = membership_test(s + a + dernier)
-                pref[s + a] = "blue"
-    return mq, pref, exp
-
-
+# Carla : je ne sais pas à quelle méthode cela correspond
 epsilon = 0
-alphabet = {'a','b'}
-
-
-def Lstar_Initialise():
-    red = []
-    red.append('epsilon')
-    blue = []
-    for l in alphabet:
-        blue.append(l)
-    exp = []
-    exp.append('epsilon')
-    n=len(red) # nombre de lignes
-    m=len(exp) # nombre de colonnes
-    M = 'teste d equivalence'
-    table = [[ '' for j in range(m)] for i in range(n)]
-    print(table)
-    table['epsilon']['epsilon'] = M('epsilon')
-    for lettre in alphabet:
-        table[lettre]['epsilon'] = M(lettre)
-    return table
-
-
-print(Lstar_Initialise())
-
-#A implementer, demander a Harry
-def equivalence_test(mq, pref, exp):
-    return True
+alphabet = {'a', 'b'}
 
 
 def lstar():
@@ -209,11 +214,11 @@ def lstar():
     while a or answer:
         a = False
         while not is_closed(mq, pref, exp) or not is_consistent(mq, pref, exp):
-            if not is_closed(mq, pref, exp) :
+            if not is_closed(mq, pref, exp):
                 mq, pref, exp = lstar_close(mq, pref, exp, alphabet)
             if not is_consistent(mq, pref, exp):
-                mq, pref, exp = lstar_consistent(mq, pref, exp, alphabet)
+                mq, pref, exp = lstar_consistent(mq, pref, exp)  # Carla :cette méthode ne retourne rien...
         answer = equivalence_test(mq, pref, exp)
-        if answer != True :
-            mq, pref, exp = LSTAR_USEEQ(mq, pref, exp)
+        if answer != True:
+            mq, pref, exp = LSTAR_USEEQ(mq, pref, exp)  # Carla :cette méthode ne retourne rien pour l'instant
     return lstar_buildautomaton(mq, pref, exp, alphabet)
