39d0d0a8056404a4491cc6ff5e9d654aad3e750f
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-03-14 23:03:28 +0100

    res test consistent

--- main.py
+++ main.py
@@ -46,15 +46,21 @@ exp2 = ['']
 
 
 mq = {'': 1, 'a': 0, 'aa': 1, 'b': 0, 'ba': 0, 'bb': 1, 'bba': 0, 'aaa': 0, 'ab': 0, 'aba': 0, 'baa': 0, 'bbaa': 1, 'bbb': 0, 'bbba': 0}
-pref = {'': 'RED', 'a': 'RED', 'b': 'RED', 'bb': 'RED', 'aa': 'BLUE', 'ab': 'BLUE', 'ba': 'BLUE', 'bba': 'BLUE', 'bbb': 'BLUE'}
+pref = {'': 'red', 'a': 'red', 'b': 'red', 'red': 'red', 'aa': 'blue', 'ab': 'blue', 'ba': 'blue', 'bba': 'blue', 'bbb': 'blue'}
 exp = ['', 'a']
+
+
 
 before_automate = Angluin(automate_test.input_symbols, automate_test, mq2, pref2, exp2)
 learned_automate = Angluin(automate_test.input_symbols, automate_test, mq, pref, exp)
 # print(learned_automate.is_consistent())
 # print(before_automate.is_consistent())
 before_automate.lstar_consistent()
-print("mq : ", mq, "pref : ", pref, "exp : ", exp)
+print("mq : ", mq2, "pref : ", pref2, "exp : ", exp2)
+# res :
+# mq2 :  {'': 1, 'a': 0, 'aa': 1, 'b': 0, 'bb': 1, 'ab': 0, 'ba': 0, 'bba': 0, 'bbb': 0, 'aaa': 1, 'aba': 0, 'baa': 0, 'bbaa': 1, 'bbba': 0}
+# pref2 :  {'': 'red', 'a': 'red', 'b': 'red', 'bb': 'red', 'aa': 'blue', 'ab': 'blue', 'ba': 'blue', 'bba': 'blue', 'bbb': 'blue'}
+# exp2 :  ['', 'a']
 
 
 
