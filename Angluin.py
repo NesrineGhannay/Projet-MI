cc864de830fc953c05d456584212b3100549813c
carla <carla.segalie@etu.univ-amu.fr>
2023-03-14 16:12:43 +0100

    Ajout des self

--- Angluin.py
+++ Angluin.py
@@ -28,9 +28,9 @@ class Angluin:
             table[lettre]['epsilon'] = M(lettre)
         return table
 
-    def compareOT(self, mq, exp, u, v):
-        for e in exp:
-            if mq[str(u + e)] != mq[str(v + e)]:
+    def compareOT(self, u, v):
+        for e in self.exp:
+            if self.mq[str(u + e)] != self.mq[str(v + e)]:
                 return False
         return True
 
@@ -40,32 +40,32 @@ class Angluin:
     output = the updating table corresponding to the new actual automaton
     -- uses function membership_test
     """
-    def blue(self, pref):
+    def blue(self):
         blue = []
-        for i in pref:
-            if pref[i] == "blue":
+        for i in self.pref:
+            if self.pref[i] == "blue":
                 blue.append(i)
         return blue
 
-    def red(self, pref):
+    def red(self):
         red = []
-        for i in pref:
-            if pref[i] == "red":
+        for i in self.pref:
+            if self.pref[i] == "red":
                 red.append(i)
         return red
 
-    def is_closed(self, mq, pref, exp):
-        for s in self.blue(pref):
-            for u in self.red(pref):
-                if not self.compareOT(mq, exp, s, u):
+    def is_closed(self):
+        for s in self.blue():
+            for u in self.red():
+                if not self.compareOT(s, u):
                     return False
         return True
 
 
-    def different(self, mq, pref, exp, s):
-        dernier = exp[len(exp) - 1]
-        for u in self.red(pref):
-            if mq[s + dernier] == mq[u + dernier]:
+    def different(self, s):
+        dernier = self.exp[len(self.exp) - 1]
+        for u in self.red(self.pref):
+            if self.mq[s + dernier] == self.mq[u + dernier]:
                 return False
         return True
 
@@ -84,12 +84,12 @@ class Angluin:
     def lstar_close(self):
         dernier = self.exp[len(self.exp) - 1]
         for s in self.blue(self.pref):
-            if self.different(mq, pref, exp, s):
-                pref[s] = "red"
-                for a in alphabet:
-                    mq[s + a + dernier] = self.membership_test(s + a + dernier)
-                    pref[s + a] = "blue"
-        return mq, pref, exp
+            if self.different(s):
+                self.pref[s] = "red"
+                for a in self.alphabet:
+                    self.mq[s + a + dernier] = self.membership_test(s + a + dernier)
+                    self.pref[s + a] = "blue"
+        return self.mq, self.pref, self.exp
 
 
     """
@@ -98,13 +98,13 @@ class Angluin:
     output = [False, (a,e)] if the word (a+e) make the table not consistent
              [True] if the table is consistent
     """
-    def find_consistency_problem(self, mq, pref, exp):
-        for s1 in self.red(pref):
-            for s2 in self.red(pref):
-                if self.compareOT(mq, exp, s1, s2):
-                    for e in exp:
+    def find_consistency_problem(self):
+        for s1 in self.red():
+            for s2 in self.red():
+                if self.compareOT(s1, s2):
+                    for e in self.exp:
                         for a in self.alphabet:
-                            if mq[str(s1 + a + e)] != mq[str(s2 + a + e)]:
+                            if self.mq[str(s1 + a + e)] != self.mq[str(s2 + a + e)]:
                                 return [False, (a, e)]
         return [True]
 
@@ -116,7 +116,7 @@ class Angluin:
              True  (if find_consistency_problem return only [True] the table is consistent)
     """
     def is_consistent(self, mq, pref, exp):
-        res = self.find_consistency_problem(self, mq, pref, exp)
+        res = self.find_consistency_problem()
         if res[0]:
             return True
         return False
@@ -127,15 +127,15 @@ class Angluin:
     output = the updating table corresponding to the new actual automaton
     -- uses function membership_test
     """
-    def lstar_consistent(self, mq, pref, exp):
-        a, e = self.find_consistency_problem(mq, pref, exp)[1]
-        exp.add(str(a + e))
-        for line in pref:
-            for e in exp:
-                if str(line + e) not in mq:
+    def lstar_consistent(self):
+        a, e = self.find_consistency_problem()[1]
+        self.exp.add(str(a + e))
+        for line in self.pref:
+            for e in self.exp:
+                if str(line + e) not in self.mq:
                     self.fill_the_table(self, str(line + e))
 
-    def equivalence_test(self, mq, pref, exp):
+    def equivalence_test(self):
         return True
 
     """
@@ -147,20 +147,20 @@ class Angluin:
             prefixes.append(word[0:i])
         return prefixes
 
-    def LSTAR_USEEQ(self, mq, pref, exp, answer):
+    def LSTAR_USEEQ(self, answer):
         prefixes = self.get_prefixes(answer)
         for p in prefixes:
-            pref[p] = "red"
+            self.pref[p] = "red"
             for a in self.alphabet:
                 if str(p + a) not in prefixes:
-                    pref[str(p + a)] = "blue"
+                    self.pref[str(p + a)] = "blue"
         # for line in pref.keys():
         # for line in list(pref.keys()):
-        for line in [*pref]:
-            for e in exp:
-                if str(line + e) not in mq:
-                    # mq[str(line + e)] = self.membership_test(
-                    #     str(line + e))  # nom temporaire selon comment on fait pour les requetes d'appartenance
+        for line in [*self.pref]:
+            for e in self.exp:
+                if str(line + e) not in self.mq:
+                    # self.mq[str(line + e)] = self.membership_test(
+                    # str(line + e))  # nom temporaire selon comment on fait pour les requetes d'appartenance
         # return mq, pref, exp # j'allais le mettre mais enft ça modifie direct je pense (j'espère)
                     self.fill_the_table(self, str(line+e))
 
@@ -169,14 +169,13 @@ class Angluin:
     Input = the table corresponding to the actual automaton
     Output = the automaton corresponding to the table
     """
-
-    def lstar_build_automaton(self, mq, pref, exp):
+    def lstar_build_automaton(self):
         states = set()
-        red = self.red(pref)
+        red = self.red()
         for u in red:
             state = True
             for v in states:
-                if self.compareOT(mq, exp, u, v):
+                if self.compareOT(u, v):
                     state = False
                     break
             if state:
@@ -184,20 +183,16 @@ class Angluin:
         final_states = set()
         transitions = {}
         for state in states:
-            if mq[state] == 1:
+            if self.mq[state] == 1:
                 final_states.add(state)
             transitions[state] = {}
             for letter in self.alphabet:
                 x = state + letter
                 for other_state in states:
-                    if self.compareOT(mq, exp, x, other_state):
+                    if self.compareOT(x, other_state):
                         transitions[state][letter] = other_state
                         break
         return DFA(states=states, input_symbols=self.alphabet, transitions=transitions, initial_state="", final_states=final_states)
-
-    # Carla : je ne sais pas à quelle méthode cela correspond
-    epsilon = 0
-    alphabet = {'a', 'b'}
 
     def lstar(self):
         mq, pref, exp = self.Lstar_Initialise()
