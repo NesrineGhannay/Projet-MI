4052df4f52cb961c4fb99f168a5081ab743acee7
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-04-13 14:41:25 +0200

    Ajout de la méthode completedAutomata

--- Compositional_Model_Checking.py
+++ Compositional_Model_Checking.py
@@ -1,3 +1,4 @@
+from automata.fa.dfa import DFA
 
 
 def restriction(mot, alphabet):
@@ -10,9 +11,31 @@ def restriction(mot, alphabet):
 
 """
 On complète l'automate de base par un état puits qui est un état d'erreur (P_err = nouvel automate)
-input = Automate à compléter
-output = Automate dans lequel on a ajouté l'état d'erreur.
+input = Automate de la propriété à compléter P
+output = Automate P dans lequel on a ajouté l'état d'erreur = P_err.
 """
-def completedAutomata(automate):
+def completedAutomata(states, alphabet, transitions, initial_state, final_states):
 
-    return automate
+    ajouter_pi = False
+    for dico in transitions:
+        for char in alphabet:
+            if char not in transitions[dico]:
+                transitions[dico][char] = "pi"
+                ajouter_pi = True
+                if "pi" not in states:
+                    states.add("pi")
+    if ajouter_pi :
+        transitions["pi"] = {}
+        for char in alphabet :
+            transitions["pi"][char] = "pi"
+    return DFA(states=states, input_symbols=alphabet, transitions=transitions, initial_state=initial_state, final_states=final_states)
+
+
+# TEST
+# etats ={"0", "1"}
+# input_symbols={"b", "a"}
+# transition={"0" : {"b" : "1"}, "1" : {"a" : "0", "b" : "1"}}
+# ini_state="0"
+# fin_states={"1"}
+#
+# print(completedAutomata(etats, input_symbols, transition, ini_state, fin_states))
