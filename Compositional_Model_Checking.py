82131479f015d1aae114cb5d48aecc115a5e9bf6
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-04-13 12:12:24 +0200

    déclaration

--- Compositional_Model_Checking.py
+++ Compositional_Model_Checking.py
@@ -1,6 +1,18 @@
+
+
 def restriction(mot, alphabet):
     motRestreint = ""
     for caractere in mot:
         if caractere in alphabet:
             motRestreint += caractere
     return motRestreint
+
+
+"""
+On complète l'automate de base par un état puits qui est un état d'erreur (P_err = nouvel automate)
+input = Automate à compléter
+output = Automate dans lequel on a ajouté l'état d'erreur.
+"""
+def completedAutomata(automate):
+
+    return automate
