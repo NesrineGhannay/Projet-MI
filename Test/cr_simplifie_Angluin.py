b109a5bc29b2465515b0c7bbc0112102038d09c8
carla <carla.segalie@etu.univ-amu.fr>
2023-05-04 15:57:28 +0200

    MAJ test

--- Test/cr_simplifie_Angluin.py
+++ Test/cr_simplifie_Angluin.py
@@ -11,19 +11,20 @@ final_states = {0}
 abscisse = []
 ordonnee = []
 
-for i in range(1, 100):
-    abscisse.append(i+1)
+
+for i in range(0, 2000001, 250000):
+    abscisse.append(i+2)
     automate = DFA(states=states, input_symbols= alphabet, transitions=transitions, initial_state=initial_state, final_states=final_states)
     a = time.time()
     angluin = Angluin(alphabet=alphabet, automate_a_apprendre=automate, mq={}, pref={}, exp=[])
     angluin.lstar()
     b = time.time()
     ordonnee.append(b-a)
-    states.add(i)
-    transitions[i - 1] = {"a": i, "b": i}
-    transitions[i] = {"a": "puits", "b": "puits"}
-    final_states = {i}
-
+    for j in range(250000):
+        states.add(i+j+1)
+        transitions[i+j] = {"a": i+j+1, "b": i+j+1}
+        transitions[i+j+1] = {"a": "puits", "b": "puits"}
+        final_states = {i+j+1}
 
 plt.figure()
 plt.plot(abscisse, ordonnee)
