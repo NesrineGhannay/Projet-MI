cf54b80e51d43a8165ad01d5bf5da09f04aa0b84
carla <carla.segalie@etu.univ-amu.fr>
2023-05-04 20:29:03 +0200

    renommage dans cr_simplifie_Angluin.py

--- Test/cr_simplifie_Angluin.py
+++ Test/cr_simplifie_Angluin.py
@@ -8,18 +8,18 @@ initial_state = 0
 final_states = {0}
 
 
-abscisse = []
-ordonnee = []
+abscissa = []
+ordinate = []
 
 
 for i in range(0, 2000001, 250000):
-    abscisse.append(i+2)
-    automate = DFA(states=states, input_symbols= alphabet, transitions=transitions, initial_state=initial_state, final_states=final_states)
+    abscissa.append(i+2)
+    automaton = DFA(states=states, input_symbols= alphabet, transitions=transitions, initial_state=initial_state, final_states=final_states)
     a = time.time()
-    angluin = Angluin(alphabet=alphabet, automate_a_apprendre=automate, mq={}, pref={}, exp=[])
+    angluin = Angluin(alphabet=alphabet, automate_a_apprendre=automaton, mq={}, pref={}, exp=[])
     angluin.lstar()
     b = time.time()
-    ordonnee.append(b-a)
+    ordinate.append(b-a)
     for j in range(250000):
         states.add(i+j+1)
         transitions[i+j] = {"a": i+j+1, "b": i+j+1}
@@ -27,7 +27,7 @@ for i in range(0, 2000001, 250000):
         final_states = {i+j+1}
 
 plt.figure()
-plt.plot(abscisse, ordonnee)
+plt.plot(abscissa, ordinate)
 plt.xlabel("Nombre d\'états")
 plt.ylabel('Temps d\'exécution d\'Angluin en secondes')
 plt.title('Complexité de temps d\'Angluin')
