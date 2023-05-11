1b5b991e6790705c9245a09eab6e970c907ee229
carla <carla.segalie@etu.univ-amu.fr>
2023-05-11 21:10:31 +0200

    test_parallel_composition ok

--- Test/test_Compositional_Model_Checking.py
+++ Test/test_Compositional_Model_Checking.py
@@ -2,48 +2,52 @@ from Compositional_Model_Checking import *
 import pytest
 from automata.fa.dfa import DFA
 
+
 @pytest.fixture
 def Input():
     return DFA(
-    states={"0", "1", "2"},
-    input_symbols={"a", "i", "s"},
-    transitions={
-        "0": {"i": "1"},
-        "1": {"s": "2"},
-        "2": {"a": "0"}
-    },
-    initial_state="0",
-    final_states={"0", "1", "2"},
-    allow_partial=True
-)
+        states={"0", "1", "2"},
+        input_symbols={"a", "i", "s"},
+        transitions={
+            "0": {"i": "1"},
+            "1": {"s": "2"},
+            "2": {"a": "0"}
+        },
+        initial_state="0",
+        final_states={"0", "1", "2"},
+        allow_partial=True
+    )
+
 
 @pytest.fixture
 def Output():
     return DFA(
-    states={"0", "1", "2"},
-    input_symbols={"a", "o", "s"},
-    transitions={
-        "0": {"s": "1"},
-        "1": {"o": "2"},
-        "2": {"a": "0"}
-    },
-    initial_state="0",
-    final_states={"0", "1", "2"},
-    allow_partial=True
-)
+        states={"0", "1", "2"},
+        input_symbols={"a", "o", "s"},
+        transitions={
+            "0": {"s": "1"},
+            "1": {"o": "2"},
+            "2": {"a": "0"}
+        },
+        initial_state="0",
+        final_states={"0", "1", "2"},
+        allow_partial=True
+    )
+
 
 @pytest.fixture
-def P() :
+def P():
     return completedAutomata(
-    states={"0", "1"},
-    alphabet={"i", "o"},
-    transitions={
-        "0": {"i": "1"},
-        "1": {"o": "0"}
-    },
-    initial_state="0",
-    final_states={"0", "1"}
-)
+        states={"0", "1"},
+        alphabet={"i", "o"},
+        transitions={
+            "0": {"i": "1"},
+            "1": {"o": "0"}
+        },
+        initial_state="0",
+        final_states={"0", "1"}
+    )
+
 
 @pytest.mark.parametrize("word, alphabet, exp",
                          [("oiaubav", {"a", "b"}, "aba"),
@@ -56,7 +60,7 @@ def test_restriction(word, alphabet, exp):
 @pytest.mark.parametrize("states, alphabet, transitions, initial_state, final_states, exp_s, exp_t",
                          [({"0", "1"}, {"i", "o"}, {"0": {"i": "1"}, "1": {"o": "0"}}, "0", {"0", "1"},
                            {"0", "1", "puits"}, {"0": {"i": "1", "o": "puits"}, "1": {"o": "0", "i": "puits"},
-                                                 "puits" : {"o" : "puits", "i" : "puits"}})])
+                                                 "puits": {"o": "puits", "i": "puits"}})])
 def test_completedAutomata(states, alphabet, transitions, initial_state, final_states, exp_s, exp_t):
     result = completedAutomata(states, alphabet, transitions, initial_state, final_states)
     assert result.__eq__(
@@ -66,24 +70,39 @@ def test_completedAutomata(states, alphabet, transitions, initial_state, final_s
 
 def test_completedAutomataByDFA(Input):
     assert completedAutomataByDFA(Input).__eq__(DFA(states={"0", "1", "puits"}, input_symbols={"i", "o"},
-                               transitions={"0": {"i": "1", "o":"puits"}, "1": {"o": "0", "i":"puits"}, "puits" : {"o" : "puits", "i" : "puits"}},
-                               initial_state="0", final_states={"0", "1"}))
+                                                    transitions={"0": {"i": "1", "o": "puits"},
+                                                                 "1": {"o": "0", "i": "puits"},
+                                                                 "puits": {"o": "puits", "i": "puits"}},
+                                                    initial_state="0", final_states={"0", "1"}))
 
-#print transitions pas necessaire
+
+# print transitions pas necessaire
 
 
 def test_synchronisation(Input, Output):
-    assert synchronization(Input, Output) == {("1", "0") : {"s" : ("2", "1")}, ("2", "2") : {"a" : ("0", "0")}}
+    assert synchronization(Input, Output) == {("1", "0"): {"s": ("2", "1")}, ("2", "2"): {"a": ("0", "0")}}
 
 
 def test_interleaving(Input, Output):
-    assert interleaving({("1", "0") : {"s" : ("2", "1")}, ("2", "2") : {"a" : ("0", "0")}}, Input, Output) == \
-           {("1", "0") : {"s" : ("2", "1")}, ("2", "2") : {"a" : ("0", "0")}, ("0", "0") : {"i" : ("1", "0")},
-            ("2", "1") : {"o" : ("2", "2")},
-            ('0', '1'): {'i': ('1', '1'), 'o': ('0', '2')}, ('0', '2'): {'i': ('1', '2')}, ('1', '1'): {'o': ('1', '2')}, }
+    assert interleaving({("1", "0"): {"s": ("2", "1")}, ("2", "2"): {"a": ("0", "0")}}, Input, Output) == \
+           {("1", "0"): {"s": ("2", "1")}, ("2", "2"): {"a": ("0", "0")}, ("0", "0"): {"i": ("1", "0")},
+            ("2", "1"): {"o": ("2", "2")},
+            ('0', '1'): {'i': ('1', '1'), 'o': ('0', '2')}, ('0', '2'): {'i': ('1', '2')},
+            ('1', '1'): {'o': ('1', '2')}, }
 
 
 def test_get_final_states(Input, Output):
     assert get_final_states(Input.states, Output.final_states) == {("0", "0"), ("0", "1"), ("0", "2"),
                                                                    ("1", "0"), ("1", "1"), ("1", "2"),
                                                                    ("2", "0"), ("2", "1"), ("2", "2")}
+
+
+def test_parallel_composition(Input, Output):
+    assert parallel_composition(Input, Output) == DFA(
+        states={("0", "0"), ("1", "0"), ("2", "1"), ("2", "2")},
+        input_symbols={"a", "o", "i", "s"},
+        transitions={("0", "0"): {"i": ("1", "0")}, ("1", "0"): {"s": ("2", "1")}, ("2", "1"): {"o": ("2", "2")},
+                     ("2", "2"): {"a": ("0", "0")}},
+        initial_state=("0", "0"),
+        final_states={("0", "0"), ("1", "0"), ("2", "1"), ("2", "2")},
+        allow_partial=True)
